4d35158eecd4c5f4883c36173c7810090faf46d2
takumi19910112 <114665284+takumi19910112@users.noreply.github.com>
2022-10-06 17:22:55 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root to: "rooms#index"
 
   resources :users
-  resources :rooms
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
